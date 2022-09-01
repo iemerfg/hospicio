24ff020ed9786931963e0a5f4651440a83540a67
iemerfg <iemerfg2016@gmail.com>
2022-09-01 09:03:18 -0300

    Estilização dos formulários

--- hospicio/settings.py
+++ hospicio/settings.py
@@ -118,8 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    'static',
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
